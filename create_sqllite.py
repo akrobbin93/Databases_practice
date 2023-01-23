e1830a9724305ef6dd4b41e9325edda7314f9b6d
akrobbin93 <akylerobbins@gmail.com>
2023-01-23 11:59:56 -0500

    in SQLite, create movies.db and add movies to it, and filter out by release year

--- create_sqllite.py
+++ create_sqllite.py
@@ -7,5 +7,24 @@ cursor = connection.cursor()
 cursor.execute('''CREATE TABLE IF NOT EXISTS Movies
     (Title TEXT, Director TEXT, Year INT)''')
 
+cursor.execute("INSERT INTO Movies VALUES ('Taxi Driver', 'Martin Scorsese', 1976)")
+
+cursor.execute('SELECT * FROM Movies')
+
+famousFilms = [('Pulp Fiction', 'Quentin Tarantino', 1994),
+('Back to the Future', 'Steven Spielberg', 1985),
+('Moonrise Kingdom', 'Wes Anderson', 2012)]
+
+cursor.executemany('INSERT INTO Movies VALUES (?,?,?)', famousFilms)
+
+records = cursor.execute('SELECT * FROM Movies')
+
+print(cursor.fetchall())
+
+release_year = (1985,)
+cursor.execute("SELECT * FROM Movies WHERE year=?", release_year)
+
+print(cursor.fetchall())
+
 connection.commit()
 connection.close()
